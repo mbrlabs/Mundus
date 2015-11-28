ef96c31e3b5b1f3cd758070d80f0debd9e638e8c
Marcus Brummer <mbrlabs7@gmail.com>
2015-11-28 21:14:54 +0100

    settings dialog: fbx-conv binary

--- editor/src/com/mbrlabs/mundus/ui/components/dialogs/SettingsDialog.java
+++ editor/src/com/mbrlabs/mundus/ui/components/dialogs/SettingsDialog.java
@@ -1,10 +1,11 @@
 package com.mbrlabs.mundus.ui.components.dialogs;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
-import com.kotcrab.vis.ui.widget.VisLabel;
-import com.kotcrab.vis.ui.widget.VisSplitPane;
-import com.kotcrab.vis.ui.widget.VisTable;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.kotcrab.vis.ui.widget.*;
+import com.mbrlabs.mundus.data.home.MundusHome;
+import com.mbrlabs.mundus.data.home.Settings;
 
 /**
  * @author Marcus Brummer
@@ -16,8 +17,18 @@ public class SettingsDialog extends BaseDialog {
     private VerticalGroup settingsSelection;
     private VisTable content;
 
+    private VisTextField path;
+
+    private VisTextButton save;
+
     public SettingsDialog() {
         super("Settings");
+
+        VisTable root = new VisTable();
+        root.padTop(6).padRight(6).padBottom(22);
+        add(root);
+        //root.debug();
+
         settingsSelection = new VerticalGroup();
         settingsSelection.addActor(new VisLabel("General"));
         settingsSelection.addActor(new VisLabel("Terrain"));
@@ -25,9 +36,39 @@ public class SettingsDialog extends BaseDialog {
         settingsSelection.addActor(new VisLabel("Export Settings"));
 
         content = new VisTable();
+        content.top().left();
+
+        content.padTop(6).padRight(6).padLeft(6).padBottom(22);
 
         splitPane = new VisSplitPane(settingsSelection, content, false);
-        getContentTable().add(splitPane).fill().expand();
+        splitPane.setSplitAmount(0.3f);
+        root.add(splitPane).width(700).minHeight(400).fill().expand();
+
+        content.add(new VisLabel("fbx-conv:")).padRight(5);
+        path = new VisTextField();
+        content.add(path).width(300).row();
+
+        save = new VisTextButton("Save");
+        content.add(save).width(93).height(25).padTop(15).colspan(2);
+
+        addHandlers();
+    }
+
+    public void reloadSettings() {
+        Settings settings = MundusHome.getInstance().getSettings();
+        path.setText(settings.getFbxConvBinary());
+    }
+
+    private void addHandlers() {
+        save.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                String fbxPath = path.getText();
+                MundusHome.getInstance().getSettings().setFbxConvBinary(fbxPath);
+                MundusHome.getInstance().save();
+            }
+        });
     }
 
 }
--- editor/src/com/mbrlabs/mundus/ui/handler/menu/MenuSettingsHandler.java
+++ editor/src/com/mbrlabs/mundus/ui/handler/menu/MenuSettingsHandler.java
@@ -3,6 +3,7 @@ package com.mbrlabs.mundus.ui.handler.menu;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.mbrlabs.mundus.ui.Ui;
+import com.mbrlabs.mundus.ui.components.dialogs.SettingsDialog;
 
 /**
  * @author Marcus Brummer
@@ -13,7 +14,9 @@ public class MenuSettingsHandler extends ChangeListener {
     @Override
     public void changed(ChangeEvent event, Actor actor) {
         final Ui ui = Ui.getInstance();
-        ui.showDialog(ui.getSettingsDialog());
+        SettingsDialog dialog = ui.getSettingsDialog();
+        dialog.reloadSettings();
+        ui.showDialog(dialog);
     }
 
 }
