66c7ca5a4097ff0166a148e5b4e3e5f85d1059d8
Marcus Brummer <mbrlabs7@gmail.com>
2015-12-01 19:45:29 +0100

    AddTerrainDialog can now generate a flat terrain...not very visual yet. One has to type all 4 corners of the terrain without preview

--- editor/src/com/mbrlabs/mundus/ui/components/dialogs/AddTerrainDialog.java
+++ editor/src/com/mbrlabs/mundus/ui/components/dialogs/AddTerrainDialog.java
@@ -1,12 +1,19 @@
 package com.mbrlabs.mundus.ui.components.dialogs;
 
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Container;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.kotcrab.vis.ui.VisUI;
 import com.kotcrab.vis.ui.widget.VisLabel;
 import com.kotcrab.vis.ui.widget.VisTable;
 import com.kotcrab.vis.ui.widget.VisTextButton;
 import com.kotcrab.vis.ui.widget.VisTextField;
+import com.mbrlabs.mundus.Mundus;
+import com.mbrlabs.mundus.terrain.Terrain;
+import com.mbrlabs.mundus.utils.Log;
 
 /**
  * @author Marcus Brummer
@@ -70,6 +77,63 @@ public class AddTerrainDialog extends BaseDialog {
 
     private void setupListeners() {
 
+        // generate btn
+        generateBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                try {
+                    String nom = name.getText();
+                    int vertsX = Integer.valueOf(verticesOnX.getText());
+                    int vertsZ = Integer.valueOf(verticesOnZ.getText());
+
+                    Vector3 c00 = parseCorner(corner00.getText());
+                    Vector3 c01 = parseCorner(corner01.getText());
+                    Vector3 c10 = parseCorner(corner10.getText());
+                    Vector3 c11 = parseCorner(corner11.getText());
+
+                    if(c00 != null && c01 != null && c10 != null && c11 != null) {
+                        Terrain terrain = generateTerrain(nom, c00, c01, c10, c11, vertsX, vertsZ);
+                        Mundus.projectContext.terrains.add(terrain);
+                    }
+
+                } catch (NumberFormatException nfe) {
+                    Log.error(TAG, nfe.getMessage());
+                }
+
+                //Terrain terrain = new Terrain();
+            }
+        });
+
+    }
+
+    private Vector3 parseCorner(String input) {
+        if(input == null || input.isEmpty())  return null;
+
+        String[] split = input.split(",");
+        if(split.length != 3) return null;
+
+        try {
+            float x = Float.valueOf(split[0]);
+            float y = Float.valueOf(split[1]);
+            float z = Float.valueOf(split[2]);
+            return new Vector3(x, y, z);
+        } catch (NumberFormatException nfe) {
+            Log.error(TAG, nfe.getMessage());
+            return null;
+        }
+    }
+
+    private Terrain generateTerrain(String name, Vector3 c00, Vector3 c01, Vector3 c10, Vector3 c11,
+                                 int vertCountX, int vertCountZ) {
+        Terrain terrain = new Terrain(vertCountX, vertCountZ, VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+        terrain.corner00.set(c00);
+        terrain.corner01.set(c01);
+        terrain.corner10.set(c10);
+        terrain.corner11.set(c11);
+        terrain.update();
+
+        return terrain;
     }
 
 }
