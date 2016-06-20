b008c909383ea4b1cdc1578b392f38c3c059bbfe
Marcus Brummer <mbrlabs7@gmail.com>
2016-06-20 22:17:49 +0200

    Implemented basic perlin noise

--- commons/src/com/mbrlabs/mundus/commons/terrain/Terraform.java
+++ commons/src/com/mbrlabs/mundus/commons/terrain/Terraform.java
@@ -24,10 +24,14 @@ import java.nio.ByteBuffer;
 import java.util.Random;
 
 /**
+ * Provides static methods to manipulate the height of terrains.
+ *
  * @author Marcus Brummer
  * @version 20-06-2016
  */
 public class Terraform {
+
+    private static final Random rand = new Random();
 
     /**
      * Applies perlin noise to the terrain.
@@ -35,11 +39,57 @@ public class Terraform {
      * @param terrain   terrain to which the perlin noise should be applied.
      */
     public static void perlin(Terrain terrain, float amplitude, int seed) {
+        rand.setSeed(seed);
+
         for(int i = 0; i < terrain.heightData.length; i++) {
-            terrain.heightData[i] = new Random().nextFloat() * 50;
+            int x = i % terrain.vertexResolution;
+            int z = (int) Math.floor((double)i / terrain.vertexResolution);
+
+            float height = getInterpolatedNoise(seed, x / 8f, z / 8f);
+            height *= amplitude;
+
+            terrain.heightData[z * terrain.vertexResolution + x] = height;
         }
 
         terrain.update();
+    }
+
+    private static float interpolate(float a, float b, float blend) {
+        double theta = blend * Math.PI;
+        float f = (float) (1f - Math.cos(theta)) * 0.5f;
+        return a * (1f - f) + b * f;
+    }
+
+    private static float getNoise(long seed, int x, int z) {
+        rand.setSeed(x * 49632 + z * 325176 + seed);
+        return rand.nextFloat() * 2f - 1f;
+    }
+
+    private static float getInterpolatedNoise(long seed, float x, float z){
+        int intX = (int) x;
+        int intZ = (int) z;
+        float fracX = x - intX;
+        float fracZ = z - intZ;
+
+        float v1 = getSmoothNoise(seed, intX, intZ);
+        float v2 = getSmoothNoise(seed, intX + 1, intZ);
+        float v3 = getSmoothNoise(seed, intX, intZ + 1);
+        float v4 = getSmoothNoise(seed, intX + 1, intZ + 1);
+        float i1 = interpolate(v1, v2, fracX);
+        float i2 = interpolate(v3, v4, fracX);
+        return interpolate(i1, i2, fracZ);
+    }
+
+    private static float getSmoothNoise(long seed, int x, int z) {
+        float corners = getNoise(seed, x+1, z-1) + getNoise(seed, x+1, z-1) +
+                getNoise(seed, x-1, z+1) + getNoise(seed, x+1, z+1);
+        corners /= 16f;
+        float sides = getNoise(seed, x-1, z) + getNoise(seed, x+1, z) +
+                getNoise(seed, x, z-1) + getNoise(seed, x, z+1);
+        sides /= 8f;
+        float center = getNoise(seed, x, z) / 4f;
+
+        return corners + sides + center;
     }
 
     /**
