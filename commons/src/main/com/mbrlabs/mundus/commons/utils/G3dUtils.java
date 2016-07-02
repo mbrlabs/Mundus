4cca1f68e88e5d9672727be5c22baece5d907579
Marcus Brummer <mbrlabs7@gmail.com>
2016-07-02 18:52:31 +0200

    Added a g3db loader, which ignores textures defined in the g3db file

---
+++ commons/src/main/com/mbrlabs/mundus/commons/utils/G3dUtils.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016. See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.mbrlabs.mundus.commons.utils;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader;
+import com.badlogic.gdx.graphics.g3d.model.data.ModelData;
+import com.badlogic.gdx.graphics.g3d.model.data.ModelMaterial;
+import com.badlogic.gdx.utils.UBJsonReader;
+
+/**
+ * @author Marcus Brummer
+ * @version 02-07-2016
+ */
+public class G3dUtils {
+
+    public Model loadWithoutTextures(FileHandle model) {
+        G3dModelLoader loader = new G3dModelLoader(new UBJsonReader());
+        ModelData modelData = loader.loadModelData(model);
+        for(ModelMaterial mat : modelData.materials) {
+            mat.textures.clear();
+        }
+
+        return new Model(modelData);
+    }
+
+}
