8a8ee583e60f20f00aa2e811ae1514b8fd41321b
Kostyantyn Rukhlis <Kostyantyn_Rukhlis@epam.com>
2016-10-04 12:23:22 -0400

    Basic scale tool implementation.

--- editor/src/main/com/mbrlabs/mundus/tools/ScaleTool.java
+++ editor/src/main/com/mbrlabs/mundus/tools/ScaleTool.java
@@ -16,11 +16,19 @@
 
 package com.mbrlabs.mundus.tools;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.Shader;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.mbrlabs.mundus.core.project.ProjectManager;
 import com.mbrlabs.mundus.history.CommandHistory;
+import com.mbrlabs.mundus.shader.Shaders;
 import com.mbrlabs.mundus.tools.picker.GameObjectPicker;
 import com.mbrlabs.mundus.tools.picker.ToolHandlePicker;
 import com.mbrlabs.mundus.utils.Fa;
@@ -32,10 +40,12 @@ import com.mbrlabs.mundus.utils.Fa;
 public class ScaleTool extends TransformTool {
 
     public static final String NAME = "Scale Tool";
-
+    private ScaleHandle handle;
     public ScaleTool(ProjectManager projectManager, GameObjectPicker goPicker, ToolHandlePicker handlePicker, Shader shader,
                      ModelBatch batch, CommandHistory history) {
         super(projectManager, goPicker, handlePicker, shader, batch, history);
+        ModelBuilder builder = new ModelBuilder();
+        handle = new ScaleHandle(1, builder.createXYZCoordinates(1, new Material(ColorAttribute.createDiffuse(COLOR_Z)), VertexAttributes.Usage.Position));
     }
 
     @Override
@@ -68,4 +78,48 @@ public class ScaleTool extends TransformTool {
         return Fa.EXPAND;
     }
 
+    private class ScaleHandle extends ToolHandle {
+
+        private Model model;
+        private ModelInstance modelInstance;
+
+        public ScaleHandle(int id, Model model) {
+            super(id);
+            this.model = model;
+            this.modelInstance = new ModelInstance(model);
+            modelInstance.materials.first().set(idAttribute);
+        }
+
+        public void changeColor(Color color) {
+            ColorAttribute diffuse = (ColorAttribute) modelInstance.materials.get(0).get(ColorAttribute.Diffuse);
+            diffuse.color.set(color);
+        }
+
+        @Override
+        public void render(ModelBatch batch) {
+            batch.render(modelInstance);
+        }
+
+        @Override
+        public void renderPick(ModelBatch modelBatch) {
+            batch.render(modelInstance, Shaders.pickerShader);
+        }
+
+        @Override
+        public void act() {
+
+        }
+
+        @Override
+        public void applyTransform() {
+            rotation.setEulerAngles(rotationEuler.y, rotationEuler.x, rotationEuler.z);
+            modelInstance.transform.set(position, rotation, scale);
+        }
+
+        @Override
+        public void dispose() {
+            this.model.dispose();
+        }
+
+    }
 }
