abf684c4c74132ce0e5170a280787f76230d916d
Marcus Brummer <mbrlabs7@gmail.com>
2016-10-22 18:23:37 +0200

    Added missing licence headers

--- editor/src/main/java/com/mbrlabs/mundus/editor/ui/widgets/MundusMultiSplitPane.java
+++ editor/src/main/java/com/mbrlabs/mundus/editor/ui/widgets/MundusMultiSplitPane.java
@@ -1,13 +1,11 @@
-package com.mbrlabs.mundus.editor.ui.widgets;
-
 /*
- * Copyright 2014-2016 See AUTHORS file.
+ * Copyright (c) 2016. See AUTHORS file.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +13,8 @@ package com.mbrlabs.mundus.editor.ui.widgets;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package com.mbrlabs.mundus.editor.ui.widgets;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
